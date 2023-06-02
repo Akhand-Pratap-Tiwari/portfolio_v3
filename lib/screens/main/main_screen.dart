9f5e51a9310a42b8a1f3cc22190c3a9d62be0e26
Letticonion <97452909+Letticonion@users.noreply.github.com>
2023-06-02 23:41:26 +0530

    TODO: - Add my content - Improve UX/UI - Make Rounded Corners - Transparency App Bar - Project Hover Vid Preview - Lottie Banner In Bg

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -13,51 +13,56 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: Responsive.isDesktop(context)
-          ? null
-          : AppBar(
-              backgroundColor: bgColor,
-              leading: Builder(
-                builder: (context) => IconButton(
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  icon: const Icon(Icons.menu),
-                ),
-              ),
-            ),
-      drawer: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-        child: const SideMenu(),
-      ),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: maxWidth),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (Responsive.isDesktop(context))
-                const Expanded(
-                  flex: 2,
-                  child: SideMenu(),
-                ),
-              const SizedBox(width: defaultPadding),
-              Expanded(
-                flex: 7,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      ...children,
-                      // our footer
-                    ],
+      child: Scaffold(
+        appBar: Responsive.isDesktop(context)
+            ? null
+            : AppBar(
+                backgroundColor: bgColor,
+                leading: Builder(
+                  builder: (context) => IconButton(
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    icon: const Icon(Icons.menu),
                   ),
                 ),
               ),
-            ],
+        drawer: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+          child: const SideMenu(),
+        ),
+        body: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: maxWidth),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (Responsive.isDesktop(context))
+                  const Expanded(
+                    flex: 2,
+                    child: SideMenu(),
+                  ),
+                // const SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 7,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: defaultPadding),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ...children,
+                          // our footer
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
